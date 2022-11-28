a2949c64c94dc7b0620dbcf2d562b8335b3fcd01
NIHAR SARMA <sarmanihar2003@gmail.com>
2022-11-28 22:06:13 +0530

    implemented basic api fetching

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-function App() {
-  return <div className="App"></div>;
+import React from 'react'
+
+import FetchFromApi_event from './components/events'
+import FetchFromApi_member from './components/members'
+
+const App = () => {
+  return (
+    <div>
+<FetchFromApi_event/>
+<FetchFromApi_member/>
+    </div>
+  )
 }
 
-export default App;
+export default App

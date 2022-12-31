78dbd0e8e4917d2506bd072a09bd653cf37ac456
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2022-12-31 19:34:20 +0530

    Update GetEvents.jsx

--- src/services/Events/GetEvents.jsx
+++ src/services/Events/GetEvents.jsx
@@ -6,11 +6,8 @@ import ServerUrl from '../../ServerUrl';
 const GetEvents = async () => {
     try {
         const res = await axios.get(
-<<<<<<< HEAD:src/services/GetMembers.jsx
-          "http://tasty-crab-hosiery.cyclic.app/api/admin/members/21-22"
-=======
+
           `${ServerUrl}/events`
->>>>>>> 4a97023ba54b684c1753b8b899205a4ec530b612:src/services/Events/GetEvents.jsx
         );
         console.log("hello");
         // toast.success("success")

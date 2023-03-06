42558e4553a9ecf5491d80d010b102bcfe9747ca
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2023-03-07 00:54:49 +0530

    resolving merge conflit

--- src/services/Events/CreateEvents.jsx
+++ src/services/Events/CreateEvents.jsx
@@ -4,6 +4,7 @@ import { toast } from 'react-toastify';
 const CreateEvents = async (sendForm, setDataTransfer, reloadReq, setReloadReq, event) => {
   const ServerUrl = process.env.REACT_APP_SERVER_URL;
   try {
+    // console.log(sendForm);
     const res = await axios.post(
       `${ServerUrl}/${event}`,
       sendForm,
@@ -17,6 +18,7 @@ const CreateEvents = async (sendForm, setDataTransfer, reloadReq, setReloadReq, 
     setReloadReq(!reloadReq);
     return res.data;
   } catch (err) {
+    console.log(err);
     setDataTransfer(false);
     toast.error(err.message);
   }
